7960172d392b30711e15f80cb400dad3c5fc2ff4
tongming <406638191@qq.com>
2018-04-23 22:53:14 +0800

    first commit

---
+++ src/demo/FirstJava.java
@@ -0,0 +1,11 @@
+package demo;
+
+/**
+ * Created by Administrator on 2018/4/23.
+ */
+public class FirstJava
+{
+    public  static  void  main(String[] args) {
+        System.out.println(123);
+    }
+}
